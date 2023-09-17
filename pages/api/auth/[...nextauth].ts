354a5ed1e7fc44da3a25c10f4e4609957fbc15f3
Pete Gordon <pete@petegordon.com>
2023-09-17 19:58:49 -0400

    conflict in example betwen app and pages nextauth also added SupaBase adapter to persist to database the accounts and users can only use one though tested with Facebook and Google

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,8 @@ import FacebookProvider from "next-auth/providers/facebook"
 import GithubProvider from "next-auth/providers/github"
 import TwitterProvider from "next-auth/providers/twitter"
 import Auth0Provider from "next-auth/providers/auth0"
+import { SupabaseAdapter } from "@auth/supabase-adapter"
+
 // import AppleProvider from "next-auth/providers/apple"
 // import EmailProvider from "next-auth/providers/email"
 
@@ -51,6 +53,10 @@ export const authOptions: NextAuthOptions = {
       issuer: process.env.AUTH0_ISSUER,
     }),
   ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  }),
   theme: {
     colorScheme: "light",
   },
