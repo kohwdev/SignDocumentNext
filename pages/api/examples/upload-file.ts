679c1354b2b2ff8330c338c85294cb807dd488a0
Steve Horn <steve@stevehorn.cc>
2023-09-17 23:30:33 -0400

    save incoming file

--- pages/api/examples/upload-file.ts
+++ pages/api/examples/upload-file.ts
@@ -1,12 +1,39 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import * as path from "path";
-import * as fs from "fs";
-import formidable, { File } from 'formidable';
-// File is successfully submitted here from /protected. Next step is to parse multi-part form data.
-// TOOD: https://github.com/gapon2401/upload-files-nextjs/blob/master/pages/api/upload.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+import { IncomingForm } from "formidable";
+import fs from "fs";
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    if (req.method !== "POST") {
+        return;
+    }
 
+    // parse form with a Promise wrapper
+    const data = await new Promise((resolve, reject) => {
+        const form = new IncomingForm();
+        form.parse(req, (err, fields, files) => {
+            if (err) return reject(err);
+            resolve({ fields, files });
+        });
+    });
+
+    console.log("------");
+    console.log(data);
+    console.log("------");
+    console.log(data.files.resume);
+    console.log("------");
+
+    let buffer = fs.readFileSync(data.files.file[0].filepath);
+
+    fs.writeFileSync("/Users/stevehorn/Downloads/file.pdf", buffer);
+
+    res.status(200).json(data);
 }
